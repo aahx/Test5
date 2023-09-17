0b845ee0f114ccbf9d299883d2c6ab992049d82b
Michael Kim <michaelkim009@gmail.com>
2023-09-16 21:47:35 -0700

    Using TextLoader

--- Setbacks.md
+++ Setbacks.md
@@ -1,8 +1,13 @@
 # from langchain.document_loaders import UnstructuredURLLoader
 
+`https://python.langchain.com/docs/integrations/document_loaders/url`
 - Could not get UnstructuredURLLoader to work
     - Error Message:
         -   exception: Invalid file. The FileType.UNK file type is not supported in partition.
     - Attempted brew install libmagic
         - New Error: nltk_data
 
+# NLTK.download()
+- SSL certificate unverifiable
+- This affects both UnstructuredURLLoader and SeleniumURLLoader
+- As of now will be using simple TextLoader
--- main.py
+++ main.py
@@ -1,20 +1,17 @@
-# Helps load pages that require JS to render
-from langchain.document_loaders import SeleniumURLLoader 
+from langchain.document_loaders import TextLoader
+from langchain.chains.summarize import load_summarize_chain
+from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 
-from langchain.chains.summarize import load_summarize_chain  # Summarize Chain
-from langchain.llms import OpenAI  # Our Language Model
-from langchain.prompts import PromptTemplate  # Prompt Templates
-from langchain.text_splitter import RecursiveCharacterTextSplitter # Split up our Documents
-
-
-def get_company_page():   
-    urls = [
-        "https://www.tesla.com/about"
-    ]
-    loader = SeleniumURLLoader(urls=urls)
+def get_company_data():
+    loader = TextLoader("./company_data.txt")
     return loader.load()
 
+
 # Get the data of the company you're interested in
-data = get_company_page()
-print (f"You have {len(data)} document(s)")
+data = get_company_data()
+print(f"You have {len(data)} document(s)")
+
+print(f"Preview of your data:\n\n{data[0].page_content[:30]}")
