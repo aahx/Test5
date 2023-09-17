b9f5d4a971a31a00a249f12ce84aee57b7ac523a
Michael Kim <michaelkim009@gmail.com>
2023-09-17 00:46:32 -0700

    mvp with greg kamradt

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+# https://www.youtube.com/watch?v=y1pyAQM-3Bo
+# Sales Email with Greg Kamradt
+
 import os
 import constants as constants
 
@@ -22,8 +25,8 @@ def get_company_data():
     return loader.load()
 
 data = get_company_data()
-print(f"You have {len(data)} main document(s)")
-print(f"Preview of your data:\n\n{data[0].page_content[:80]}")
+# print(f"You have {len(data)} main document(s)")
+# print(f"Preview of your data:\n\n{data[0].page_content[:80]}")
 
 
 ################
@@ -46,41 +49,67 @@ docs = text_splitter.split_documents(data)
 #    combine_prompt will be the propmt that is used when you combine the outputs of your map pass
 ################
 map_prompt = """ 
-    Below is a section of a website about {prospect}
+Below is a section of a website about {prospect}
 
-    Write a concise summary about {prospect}. If the information is not about {prospect}, exclude it from your summary.
+Write a concise summary about {prospect}. If the information is not about {prospect}, exclude it from your summary.
 
-    {text}
+{text}
 
-    % CONCISE SUMMARY : 
+% CONCISE SUMMARY : 
 """
+
 map_prompt_template = PromptTemplate(template=map_prompt, input_variables=["text", "prospect"])
 
 
 combine_prompt = """
-    Your goal is to write a personalized outbound email from {sales_rep}, a sales rep at {company} to {prospect}.
+Your goal is to write a personalized outbound email from {sales_rep}, a sales rep at {company} to {prospect}.
 
-    A good email is personalized and combines information about the two companies on how they can help each other.
-    Be sure to use value selling: A sales methodology that focuses on how your product or service will provide value to 
-    the customer instead of focusing on price or solution.
+A good email is personalized and combines information about the two companies on how they can help each other.
+Be sure to use value selling: A sales methodology that focuses on how your product or service will provide value to 
+the customer instead of focusing on price or solution.
 
-    % INFORMATION ABOUT {company}:
-        {company_information}
+% INFORMATION ABOUT {company}:
+    {company_information}
 
-    % INFORMATION ABOUT {prospect}:
-        {text}
+% INFORMATION ABOUT {prospect}:
+    {text}
 
-    % INCLUDE THE FOLLOWING PIECES IN YOUR RESPONSE:
-        - Start the email with the sentence: "We love that {prospect} helps teams..." then insert what they help teams do.
-        - The sentence: "We can help you do XYZ by ABC" Replace XYZ with what {prospect} does and ABC with what {company} does 
-        - A 1-2 sentence description about {company}, be brief
-        - End your email with a call-to-action such as asking them to set up time to talk more
+% INCLUDE THE FOLLOWING PIECES IN YOUR RESPONSE:
+    - Start the email with the sentence: "We love that {prospect} helps teams..." then insert what they help teams do.
+    - The sentence: "We can help you do XYZ by ABC" Replace XYZ with what {prospect} does and ABC with what {company} does 
+    - A 1-2 sentence description about {company}, be brief
+    - End your email with a call-to-action such as asking them to set up time to talk more
 
-    % YOUR RESPONSE: 
+% YOUR RESPONSE: 
 """
 
 combine_prompt_template = PromptTemplate(
     template=combine_prompt, 
-    input_variables=["sales_rep", "company", "prospect", "text", "company_information"])
+    input_variables=["company", "company_information", "sales_rep", "prospect", "text"])
 
 
+llm = OpenAI(temperature=.5)
+# llm = OpenAI(temperature=.5, openai_api_key=openai_api_key)
+
+################
+#   Using Summarize to use map reduce in an efficient way, although I am not summarizing
+#   By using prompts (map_prompt and combine_prompt), we are giving it more direction
+################
+chain = load_summarize_chain(
+    llm,
+    chain_type="map_reduce",
+    map_prompt=map_prompt_template,
+    combine_prompt=combine_prompt_template,
+    verbose=False
+)
+
+################
+output = chain({
+    "input_documents": docs,
+    "company": "RapidRoad",
+    "company_information" : company_info,
+    "sales_rep" : "Michael",
+    "prospect" : "GitLab"
+})
+
+print(output['output_text'])
