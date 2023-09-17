1dfd659f02d72a0f9180049c7999c78aeb11fed0
Michael Kim <michaelkim009@gmail.com>
2023-09-16 21:23:44 -0700

    going with SeleniumURLLoader - having difficulties with UnstructuredURLLoader

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
-
 """
-    Could not get UnstructuredURLLoader to work. SSL Certificate Error?
+    Could not get UnstructuredURLLoader to work.
     Error Message:
         exception: Invalid file. The FileType.UNK file type is not supported in partition.
+    -- Attempted brew install libmagic
+    New Error:
+        nltk_data
 """
 from langchain.document_loaders import UnstructuredURLLoader
 
-from langchain.document_loaders import SeleniumURLLoader # helps us load pages that require JS to render
+# Helps load pages that require JS to render
+from langchain.document_loaders import SeleniumURLLoader 
+
 
 from langchain.chains.summarize import load_summarize_chain  # Summarize Chain
 from langchain.llms import OpenAI  # Our Language Model
 from langchain.prompts import PromptTemplate  # Prompt Templates
 from langchain.text_splitter import RecursiveCharacterTextSplitter # Split up our Documents
+
 
 def get_company_page():   
     urls = [
