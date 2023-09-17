30861fe8cded91ad5d83cdf157a621fd8e138567
Michael Kim <michaelkim009@gmail.com>
2023-09-16 23:57:07 -0700

    create basic rundown with leadtech.py

---
+++ leadtech.py
@@ -0,0 +1,26 @@
+"""
+    https://python.langchain.com/docs/use_cases/question_answering.html
+"""
+
+import os
+import sys
+
+import constants
+from langchain.document_loaders import TextLoader
+from langchain.indexes import VectorstoreIndexCreator
+from langchain.llms import OpenAI
+from langchain.chat_models import ChatOpenAI
+
+os.environ["OPENAI_API_KEY"] = constants.APIKEY
+
+query = sys.argv[1]
+
+# Load Your Documents
+loader = TextLoader('./company_data.txt')
+
+# Create Your Index
+# Analysis and Structures the Data so you can query against it
+index = VectorstoreIndexCreator().from_loaders([loader])
+
+# Query Your Index, with Text and ChatOpenAI
+print(index.query(query, llm=ChatOpenAI()))
--- main.py
+++ main.py
@@ -9,9 +9,19 @@ def get_company_data():
     loader = TextLoader("./company_data.txt")
     return loader.load()
 
-
 # Get the data of the company you're interested in
 data = get_company_data()
-print(f"You have {len(data)} document(s)")
+print(f"You have {len(data)} main document(s)")
+## Preview of Data
+# print(f"Preview of your data:\n\n{data[0].page_content[:30]}")
 
-print(f"Preview of your data:\n\n{data[0].page_content[:30]}")
+
+## Don't need at the moment
+# Split up text to avoid token limits
+text_splitter = RecursiveCharacterTextSplitter(
+    # Set a really small chunk size, just to show.
+    chunk_size = 800,
+    chunk_overlap  = 0
+)
+docs = text_splitter.split_documents(data)
+print (f"You now have {len(docs)} split documents")
